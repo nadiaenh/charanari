c30d1c472f5a9b43a0fd21169c57f47f4768916a
nadiaenh <nadia.enhaili@gmail.com>
2023-08-30 22:58:32 -0700

    complete clerk auth setup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, {type ReactNode} from "react";
 import {ClerkProvider} from '@clerk/nextjs'
 import {Analytics} from '@vercel/analytics/react';
+import {UserButton} from "@clerk/nextjs";
 
 interface LayoutProps {
     children: ReactNode;
@@ -11,13 +12,12 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
         <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
             <header className="text-center py-4 border-b">
                 <h1 className="text-xl">Create Character Screen</h1>
+                <UserButton afterSignOutUrl="/"/>
             </header>
-            <ClerkProvider>
-                <main className="w-full max-w-md p-4 rounded-lg shadow-md bg-white">
-                    {children}
-                    <Analytics/>
-                </main>
-            </ClerkProvider>
+            <main className="w-full max-w-md p-4 rounded-lg shadow-md bg-white">
+                {children}
+                <Analytics/>
+            </main>
             <footer className="text-center py-4 border-t">
                 <p>© {new Date().getFullYear()} Nadia Enhaili</p>
             </footer>
