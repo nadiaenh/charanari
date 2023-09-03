f692ee47c356b2ff70b18def4e971935a73bc4a3
nadiaenh <nadia.enhaili@gmail.com>
2023-09-03 01:12:37 -0700

    remove clerk in production for now

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, {type ReactNode} from "react";
 import {Analytics} from '@vercel/analytics/react';
-import {UserButton} from "@clerk/nextjs";
 import { ThemeProvider } from "~/components/theme-provider";
 
 import { Moon, Sun } from "lucide-react"
@@ -23,8 +22,6 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="flex flex-col min-h-screen relative bg-pink-50 dark:bg-gray-900">
                 <header className="w-full flex justify-between items-center p-4 absolute top-0 right-0">
-                    {/* CLERK USER ACCOUNT BUTTON */}
-                    <UserButton afterSignOutUrl="/" />
 
                     <h1 className="text-xl">Create Character Screen</h1>
 
